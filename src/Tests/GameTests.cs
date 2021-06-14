43f7bd4ebeaad2a66b7034a37beece6afc4e67fb
bharat.mane <bharat.mane@outlook.com>
2021-06-15 00:04:52 +0530

    test(*): added more approval tests to get better confidance

--- src/Tests/GameTests.cs
+++ src/Tests/GameTests.cs
@@ -23,14 +23,22 @@ namespace Tests
         {
             StringBuilder capturedOutput = new StringBuilder();
             Console.SetOut(new StringWriter(capturedOutput));
+            int startingGameId = 345;
 
+            string actualOutput = RunAGame(startingGameId, capturedOutput);
+
+            Approvals.Verify(actualOutput);
+        }
+
+        private string RunAGame(int startingGameId, StringBuilder capturedOutput)
+        {
             var aGame = new Game();
 
             aGame.Add("Chet");
             aGame.Add("Pat");
             aGame.Add("Sue");
 
-            var rand = new Random(345);
+            var rand = new Random(startingGameId);
 
             do
             {
@@ -46,8 +54,36 @@ namespace Tests
                 }
             } while (_notAWinner);
 
-           Approvals.Verify(capturedOutput);
+            return capturedOutput.ToString();
         }
 
+        [Test]
+        public void TestSeveralGames()
+        {
+            int howManyGames = 2;
+            Dictionary<int, string> gamesOutput  = RunSeveralGames(howManyGames);
+
+            Approvals.VerifyAll(gamesOutput);
+        }
+
+        private Dictionary<int, string> RunSeveralGames(int howManyGames)
+        {
+            Dictionary<int, string> gamesOutput = new Dictionary<int, string>();
+            for (int i = 0; i < howManyGames; i++)
+            {
+                StringBuilder capturedOutput = new StringBuilder();
+                Console.SetOut(new StringWriter(capturedOutput));
+
+                int startingGameId = 456 + i * 17;
+
+                string gameOutput;
+
+                gameOutput = RunAGame(startingGameId, capturedOutput);
+
+                gamesOutput.Add(i, gameOutput);
+            }
+
+            return gamesOutput;
+        }
     }
 }
