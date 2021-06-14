6217753c5521422fbac8537696d4c39c5dc55979
bharat.mane <bharat.mane@outlook.com>
2021-06-15 02:51:59 +0530

    refactor(*): added basic functionality to player class with simple tests

---
+++ src/Tests/PlayerTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Trivia;
+
+namespace Tests
+{
+    class PlayerTests
+    {
+        [Test]
+        public void Should_Win_When_CoinsAre_6()
+        {
+            Player player = new Player("Chet");
+            player.Reward(6);
+
+            Assert.AreEqual(true,player.HasWon());
+        }
+
+        [Test]
+        public void Should_Not_Win_When_CoinsAre_Less_Than_6()
+        {
+            Player player = new Player("Chet");
+            player.Reward(5);
+
+            Assert.AreEqual(false, player.HasWon());
+        }
+
+    }
+}
--- src/Trivia/Player.cs
+++ src/Trivia/Player.cs
@@ -8,5 +8,57 @@ namespace Trivia
 {
     public class Player
     {
+        private readonly string name;
+        private int position;
+        private int goldCoinsCount;
+        private bool isInPenaltyBox = false;
+        public Player(string name)
+        {
+            this.name = name;
+        }
+
+        public int Position
+        {
+            get { return position; }
+        }
+
+        void MoveTo(int newPosition)
+        {
+            this.position = newPosition;
+        }
+
+        public int GoldCoins
+        {
+            get { return goldCoinsCount; }
+        }
+        
+        public void Reward(int goldCoinsCount)
+        {
+            this.goldCoinsCount += goldCoinsCount;
+        }
+
+        public bool HasWon()
+        {
+            return goldCoinsCount >= 6;
+        }
+
+        public void EntersPenaltyBox()
+        {
+            this.isInPenaltyBox = true;
+        }
+
+        public void ExitsPenaltyBox()
+        {
+            this.isInPenaltyBox = false;
+        }
+
+        public bool IsInPenaltyBox()
+        {
+            return isInPenaltyBox;
+        }
+        public override string ToString()
+        {
+            return name;
+        }
     }
 }
