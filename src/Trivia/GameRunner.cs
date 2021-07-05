a48b9d821474963b5b24708663d8237eb590affb
bharat.mane <bharat.mane@outlook.com>
2021-07-05 19:20:27 +0530

    chors: added static keyword of main class

--- src/Trivia/GameRunner.cs
+++ src/Trivia/GameRunner.cs
@@ -3,16 +3,16 @@ using System.Collections.Generic;
 
 namespace Trivia
 {
-    public class GameRunner
+    public static class GameRunner
     {
         private static bool _notAWinner;
-        private const int NUMBER_OF_CELLS = 12;
+        private const int NumberOfCells = 12;
         private static readonly int NB_QUESTIONS = 50;
 
         public static void Main(string[] args)
         {
             List<Category> categories = new() {Category.Pop, Category.Science, Category.Sports, Category.Rock};
-            Board board = new Board(NUMBER_OF_CELLS, categories);
+            Board board = new Board(NumberOfCells, categories);
             QuestionDeck questionDeck = new QuestionDeck(NB_QUESTIONS, categories);
             PlayerList playerList = new PlayerList(new List<string>(){ "Chet", "Pat", "Sue" });
 
