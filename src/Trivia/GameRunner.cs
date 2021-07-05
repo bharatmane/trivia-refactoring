dd361d37d91c95db5ce568dfcb954635759dd0ec
bharat.mane <bharat.mane@outlook.com>
2021-07-05 18:55:09 +0530

    refactor: used new constructor

--- src/Trivia/GameRunner.cs
+++ src/Trivia/GameRunner.cs
@@ -1,18 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Trivia
 {
     public class GameRunner
     {
         private static bool _notAWinner;
-
+        private const int NUMBER_OF_CELLS = 12;
+        private static readonly int NB_QUESTIONS = 50;
         public static void Main(string[] args)
         {
-            var aGame = new Game();
+            List<Category> categories = new() {Category.Pop, Category.Science, Category.Sports, Category.Rock};
+            Board board = new Board(NUMBER_OF_CELLS, categories);
+            QuestionDeck questionDeck = new QuestionDeck(NB_QUESTIONS, categories);
+            PlayerList playerList = new PlayerList(new List<string>(){ "Chet", "Pat", "Sue" });
 
-            aGame.Add("Chet");
-            aGame.Add("Pat");
-            aGame.Add("Sue");
+            var aGame = new Game(Console.Out, board, questionDeck, playerList);
 
             var rand = new Random();
 
